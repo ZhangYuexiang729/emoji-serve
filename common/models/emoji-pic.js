bf6b97724b7237268849071885fd10611915b94a
ZhangYueXiang729 <1759886198@qq.com>
2022-09-15 14:24:38 +0800

    新增表情包返回数据接口与总数接口

--- common/models/emoji-pic.js
+++ common/models/emoji-pic.js
@@ -12,6 +12,52 @@ module.exports = function(Emojipic) {
         })
     
       }
+      Emojipic.getCount = (cb) => {
+
+        Emojipic.find({
+          where: {
+            parentId: 0
+          }
+        }, (err, res) => {
+          cb(err, res.length)
+        })
+    
+      }
+      Emojipic.getData = (skip,limit,cb) => {
+        let allData = []
+        Emojipic.find({
+          where: {
+            parentId: 0
+          },
+          // fetch 1st "page" with 5 entries in it
+            skip:skip,
+            limit:limit
+          
+        
+        }).then(res=>{
+          
+          var time
+          
+          for(let i=0;i<res.length;i++){
+            
+            Emojipic.find({
+              where:{
+                parentId:res[i].id
+              }
+            }).then(resOne=>{
+              res[i].list = resOne
+              allData.push(res[i])
+              clearTimeout(time)
+              time = setTimeout(() => {
+              
+                  cb(null,allData);
+                }, 100)
+
+            })
+          }
+        })
+    
+      }
       Emojipic.remoteMethod("nextServerListData", {
         accepts: [{
           arg: 'id',
@@ -26,7 +72,40 @@ module.exports = function(Emojipic) {
         http: {
           verb: 'get'
         },
-        description: "获取服务包详情"
+        description: "获取图片详情"
       })
-    
+      Emojipic.remoteMethod("getCount", {
+     
+        returns: {
+          arg: "data",
+          type: 'object',
+          root: true
+        },
+        http: {
+          verb: 'get'
+        },
+        description: "获取图片数量"
+      })
+      Emojipic.remoteMethod("getData", {
+        accepts: [{
+          arg: 'skip',
+          type: 'number',
+          required: true,
+        },
+        {
+          arg: 'limit',
+          type: 'number',
+          required: true,
+        },
+      ],
+        returns: {
+          arg: "data",
+          type: 'object',
+          root: true
+        },
+        http: {
+          verb: 'get'
+        },
+        description: "获取图片数据"
+      })
 };
